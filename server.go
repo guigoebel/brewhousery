1c49e72cd770063242766677577e519b4ba28b47
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-02 15:14:24 -0800

    refactored code

--- server.go
+++ server.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/saurabmish/Car-Inventory/handlers"
 )
 
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
-
 func main() {
-	http.HandleFunc("/", HelloWorld)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// create logger to inject into reference handler
+	l := log.New(os.Stdout, "Car API service ", log.LstdFlags)
+	// create reference to hello handler
+	helloHandler := handlers.NewHello(l)
+	// register handler with server
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", helloHandler)
+
+	// reliability pattern for server
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// ensure that service will not be blocked
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// ensure graceful shutdown of server
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	signal := <-signalChannel
+	l.Println("Received terminate, graceful shutdown", signal)
+	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutContext)
 }

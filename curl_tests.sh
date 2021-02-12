271f65eea48aff8121410044bfded0eca4f53221
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-12 10:27:03 -0800

    endpoint requests using curl

---
+++ curl_tests.sh
@@ -0,0 +1,10 @@
+# Read
+curl localhost:8080 | jq
+
+# Post
+curl localhost:8080 -d '{"name": "Tea", "description": "A nice cup of tea!", "price": 2.0, "sku": "TEA-WAT-MIL-SUG"}' | jq
+curl localhost:8080 | jq
+
+# Update
+curl localhost:8080/3 -XPUT -d '{"name": "Mocha", "description": "Chocolate-flavoured variant of latte", "price": 3.00, "sku": "COF-MOC-VAR-LAT"}' | jq
+curl localhost:8080 | jq

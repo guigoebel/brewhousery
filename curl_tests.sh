51c24217b03c01c843301f6b584e19031f03cbd3
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-12 11:19:14 -0800

    added curl requests to get spcific product

--- curl_tests.sh
+++ curl_tests.sh
@@ -1,4 +1,4 @@
-# Read
+# Read all
 curl localhost:8080 | jq
 
 # Post
@@ -8,3 +8,8 @@ curl localhost:8080 | jq
 # Update
 curl localhost:8080/3 -XPUT -d '{"name": "Mocha", "description": "Chocolate-flavoured variant of latte", "price": 3.00, "sku": "COF-MOC-VAR-LAT"}' | jq
 curl localhost:8080 | jq
+
+# Read one
+curl localhost:8080/1 | jq
+curl localhost:8080/2 | jq
+curl localhost:8080/3 | jq

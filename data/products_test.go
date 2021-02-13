c5881b0f3c06c87dbda19a9f8ea30c260d7ac36c
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-12 21:12:44 -0800

    added tst cases for product validation

--- data/products_test.go
+++ data/products_test.go
@@ -1,8 +1,11 @@
 package data
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestSKUValidation(t *testing.T) {
+func TestValidProduct(t *testing.T) {
 	p := &Product{
 		Name:        "Cappuccino",
 		Description: "Espresso-based coffee prepared with steamed milk foam",
@@ -10,9 +13,48 @@ func TestSKUValidation(t *testing.T) {
 		SKU:         "COF-CAP-MIL-FOA",
 	}
 
-	err := p.Validate()
+	v := NewValidation()
+	err := v.Validate(p)
 
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestProductWithoutName(t *testing.T) {
+	p := Product{
+		Description: "No description present ...",
+		Price:       4.50,
+		SKU:         "ABC-IJK-PQR-XYZ",
+	}
+
+	v := NewValidation()
+	err := v.Validate(p)
+	assert.Len(t, err, 1)
+}
+
+func TestInvalidSKU(t *testing.T) {
+	p := &Product{
+		Name:        "Nitro Cold Brew",
+		Description: "Nitrogen infused cold brew with super-smooth taste",
+		Price:       5.50,
+		SKU:         "NIT-COL-BRE",
+	}
+
+	v := NewValidation()
+	err := v.Validate(p)
+	assert.Len(t, err, 1)
+}
+
+func TestInvalidPrice(t *testing.T) {
+	p := &Product{
+		Name:        "Eggnog Latte",
+		Description: "Nitrogen infused cold brew with super-smooth taste",
+		Price:       0,
+		SKU:         "EGG-NOG-LAT-NUT",
+	}
+
+	v := NewValidation()
+	err := v.Validate(p)
+	assert.Len(t, err, 1)
 }

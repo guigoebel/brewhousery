79e6ee85db874a020847743e76300a778f53b080
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-12 20:41:16 -0800

    added structure, message, and errors to validation

--- data/validation.go
+++ data/validation.go
@@ -1,23 +1,68 @@
 package data
 
 import (
-	"github.com/go-playground/validator"
+	"fmt"
 	"regexp"
+
+	"github.com/go-playground/validator"
 )
 
-func (p *Product) Validate() error {
+type ValidationError struct {
+	validator.FieldError
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf(
+		"Key: '%s' Error: Field validation for '%s' failed on the '%s' tag",
+		v.Namespace(),
+		v.Field(),
+		v.Tag(),
+	)
+}
+
+type ValidationErrors []ValidationError
+
+func (v ValidationErrors) Errors() []string {
+	errs := []string{}
+	for _, err := range v {
+		errs = append(errs, err.Error())
+	}
+
+	return errs
+}
+
+type Validation struct {
+	validate *validator.Validate
+}
+
+func NewValidation() *Validation {
 	validate := validator.New()
 	validate.RegisterValidation("sku", validateSKU)
 
-	return validate.Struct(p)
+	return &Validation{validate}
+}
+
+func (v *Validation) Validate(i interface{}) ValidationErrors {
+	errs := v.validate.Struct(i)
+
+	if errs == nil {
+		return nil
+	}
+
+	var returnErrs []ValidationError
+	for _, err := range errs.(validator.ValidationErrors) {
+		ve := ValidationError{err.(validator.FieldError)}
+		returnErrs = append(returnErrs, ve)
+	}
+
+	return returnErrs
 }
 
 func validateSKU(fl validator.FieldLevel) bool {
 	re := regexp.MustCompile(`[A-Z]+-[A-Z]+-[A-Z]+-[A-Z]+`)
 	matches := re.FindAllString(fl.Field().String(), -1)
-	if len(matches) != 1 {
-		return false
+	if len(matches) == 1 {
+		return true
 	}
-
-	return true
+	return false
 }

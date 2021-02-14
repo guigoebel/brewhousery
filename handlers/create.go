9f1d5afc01604a89e0e1cddf97b48036b6dd03a8
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-14 14:45:22 -0800

    handle return value from product data

--- handlers/create.go
+++ handlers/create.go
@@ -1,13 +1,22 @@
 package handlers
 
 import (
-	"github.com/saurabmish/Coffee-Shop/data"
 	"net/http"
+
+	"github.com/saurabmish/Coffee-Shop/data"
 )
 
-func (p *Products) Add(w http.ResponseWriter, r *http.Request) {
+func (p Products) Add(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("[INFO] Endpoint for POST request")
 	product := r.Context().Value(KeyProduct{}).(data.Product)
 	p.l.Println("[DEBUG] Adding product to list")
-	data.AddProduct(&product)
+	err := data.AddProduct(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		data.ToJSON(&GenericError{Message: "Couldn't create product; ID not found ..."}, w)
+		p.l.Println("[ERROR] Unable to parse values to JSON ...", err)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	data.ToJSON(&GenericError{Message: "Product created successfully!"}, w)
 }

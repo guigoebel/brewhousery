af97606b7b76e074cc66f2cef96c7a6f85b49112
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-14 10:01:18 -0800

    added headers for testing and documentation

--- handlers/read.go
+++ handlers/read.go
@@ -8,11 +8,12 @@ import (
 
 func (p *Products) RetrieveAll(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("[INFO] Endpoint for READ request")
+	w.Header().Add("Content-Type", "application/json")
 
-	listProducts := data.GetAllProducts()
+	products := data.GetAllProducts()
 	p.l.Println("[DEBUG] Retrieved all products")
 
-	err := data.ToJSON(listProducts, w)
+	err := data.ToJSON(products, w)
 	if err != nil {
 		http.Error(w, "Unable to parse values to JSON", http.StatusInternalServerError)
 	}
@@ -20,17 +21,20 @@ func (p *Products) RetrieveAll(w http.ResponseWriter, r *http.Request) {
 
 func (p *Products) RetrieveSingle(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("[INFO] Endpoint for READ request")
+	w.Header().Add("Content-Type", "application/json")
 
 	id := getProductID(r)
 	p.l.Println("[DEBUG] Retrieved product ID: ", id)
 
 	product, err := data.GetSpecificProduct(id)
 	if err != nil {
-		http.Error(w, "Unable to find product with the given ID ...", http.StatusInternalServerError)
+		http.Error(w, "Unable to find product with the given ID ...", http.StatusNotFound)
+		return
 	}
-	p.l.Println("[DEBUG] Retrieved product data successfully!")
+
 	val := data.ToJSON(product, w)
 	if val != nil {
 		http.Error(w, "Unable to parse values to JSON", http.StatusInternalServerError)
+		return
 	}
 }

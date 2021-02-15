3729f52c751831b5c4e2583951a08e4751124bdc
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-15 14:26:38 -0800

    products.go in data package completely covered

--- data/products_test.go
+++ data/products_test.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"testing"
+	"time"
 )
 
 func TestGetAllProducts(t *testing.T) {
@@ -11,42 +12,48 @@ func TestGetAllProducts(t *testing.T) {
 	}
 }
 
-func TestGetSpecificProduct(t *testing.T) {
+func TestGetSpecificExistingProduct(t *testing.T) {
 	prod, err := GetSpecificProduct(1)
 	if prod == nil && err == ErrProductNotFound {
-		t.Errorf("Could not find product with given ID")
+		t.Errorf("Could not get product with given ID ...")
+	}
+}
+
+func TestGetSpecificNonExistingProduct(t *testing.T) {
+	prod, err := GetSpecificProduct(12)
+	if prod != nil && err == ErrProductNotFound {
+		t.Errorf("Could not get product with given ID as it doesn't exist!")
 	}
 }
 
 func TestFindExistingProductIndex(t *testing.T) {
 	got := findProductIndex(1)
 	if got == -1 {
-		t.Errorf("Could not find product with given ID")
+		t.Errorf("Could not find existing product ...")
 	}
 }
 
 func TestFindNonExistingProductIndex(t *testing.T) {
 	got := findProductIndex(10)
 	if got != -1 {
-		t.Errorf("Product doesn't exist, cannot retrieve ...")
+		t.Errorf("Could not find product as it doesn't exist!")
 	}
 }
 
 func TestDeleteExistingProduct(t *testing.T) {
 	got := DeleteProduct(2)
 	if got != nil {
-		t.Errorf("Could not delete product with given ID")
+		t.Errorf("Could not delete existing product ...")
 	}
 }
 
 func TestDeleteNonExistingProduct(t *testing.T) {
 	got := DeleteProduct(20)
 	if got != ErrProductNotFound {
-		t.Errorf("Product doesn't exist, cannot delete ...")
+		t.Errorf("Could not delete product as it doesn't exist!")
 	}
 }
 
-/*
 func TestAddProduct(t *testing.T) {
 	payload := &Product{
 		12,
@@ -57,13 +64,44 @@ func TestAddProduct(t *testing.T) {
 		time.Now().UTC().String(),
 		time.Now().UTC().String(),
 	}
-	_, err := json.Marshal(payload)
-	if err != nil {
-		t.Errorf("Cannot add product ...")
-	}
 
-	if AddProduct(payload) != nil {
+	got := AddProduct(payload)
+	want := 1
+	if got != want {
 		t.Errorf("Cannot add product ...")
 	}
 }
-*/
+
+func TestUpdateExistingProduct(t *testing.T) {
+	payload := &Product{
+		12,
+		"Honey Oatmilk Latte",
+		"Oatmilk and espresso roast combined with a hint of honey",
+		3.50,
+		"HON-OAT-LAT-ESP",
+		time.Now().UTC().String(),
+		time.Now().UTC().String(),
+	}
+
+	got := UpdateProduct(1, payload)
+	if got == ErrProductNotFound {
+		t.Errorf("Could not update product with given ID ...")
+	}
+}
+
+func TestUpdateNonExistingProduct(t *testing.T) {
+	payload := &Product{
+		12,
+		"Honey Oatmilk Latte",
+		"Oatmilk and espresso roast combined with a hint of honey",
+		3.50,
+		"HON-OAT-LAT-ESP",
+		time.Now().UTC().String(),
+		time.Now().UTC().String(),
+	}
+
+	got := UpdateProduct(12, payload)
+	if got == nil {
+		t.Errorf("Could not update product with given ID as it doesn't exist!")
+	}
+}

14974c53b1d6057bbf9a5a9356c9c13ec0e6c969
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-15 09:24:14 -0800

    tests to convert payload to product object and vice-versa

--- data/json_test.go
+++ data/json_test.go
@@ -45,3 +45,46 @@ func TestProductNamePriceAndSKUToJSON(t *testing.T) {
 	err := ToJSON(p, b)
 	assert.NoError(t, err)
 }
+
+func TestProductDetailsToJSON(t *testing.T) {
+	p := []*Product{
+		&Product{
+			Name:        "Flat White",
+			Description: "Espresso-based drink with steamed milk.",
+			Price:       2.50,
+			SKU:         "FLA-WHI-ESP-MIL",
+		},
+	}
+
+	b := bytes.NewBufferString("")
+	err := ToJSON(p, b)
+	assert.NoError(t, err)
+}
+
+func TestProductNameFromJSON(t *testing.T) {
+	payload := `{"name": "Flat White"}`
+	b := bytes.NewBufferString(payload)
+	err := ToJSON(&Product{}, b)
+	assert.NoError(t, err)
+}
+
+func TestProductNameAndPriceFromJSON(t *testing.T) {
+	payload := `{"name": "Flat White", "price": 2.50}`
+	b := bytes.NewBufferString(payload)
+	err := ToJSON(&Product{}, b)
+	assert.NoError(t, err)
+}
+
+func TestProductNamePriceAndSKUFromJSON(t *testing.T) {
+	payload := `{"name": "Flat White", "price": 2.50, "sku": "FLA-WHI-ESP-MIL"}`
+	b := bytes.NewBufferString(payload)
+	err := ToJSON(&Product{}, b)
+	assert.NoError(t, err)
+}
+
+func TestProductDetailsFromJSON(t *testing.T) {
+	payload := `{"name": "Flat White", "description":""Espresso-based drink with steamed milk."", "price": 2.50, "sku": "FLA-WHI-ESP-MIL"}`
+	b := bytes.NewBufferString(payload)
+	err := ToJSON(&Product{}, b)
+	assert.NoError(t, err)
+}

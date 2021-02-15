f8ee975447586b75464230f17f83be6964ab7604
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-14 19:32:31 -0800

    added header at end

--- handlers/delete.go
+++ handlers/delete.go
@@ -6,19 +6,24 @@ import (
 	"github.com/saurabmish/Coffee-Shop/data"
 )
 
-func (p *Products) Remove(w http.ResponseWriter, r *http.Request) {
+func (p Products) Remove(w http.ResponseWriter, r *http.Request) {
 	p.l.Println("[INFO] Endpoint for DELETE request")
+	w.Header().Add("Content-Type", "application/json")
 
 	id := getProductID(r)
 	p.l.Println("[DEBUG] Retrieved product ID: ", id)
 
 	err := data.DeleteProduct(id)
 	if err == data.ErrProductNotFound {
-		http.Error(w, "Cannot delete; Product not found ...", http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
+		data.ToJSON(&GenericError{Message: "Couldn't delete product; ID not found ..."}, w)
+		p.l.Println("[ERROR] Fetching product with given ID ...", err)
 		return
+
 	}
 	if err != nil {
 		http.Error(w, "Cannot delete product ...", http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(http.StatusNoContent)
 }

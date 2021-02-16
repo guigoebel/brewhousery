74aa9807b10c1de28501aba32488d7a9f350d7c9
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-15 18:50:18 -0800

    removed commented code

--- handlers/create.go
+++ handlers/create.go
@@ -11,12 +11,6 @@ func (p Products) Add(w http.ResponseWriter, r *http.Request) {
 	product := r.Context().Value(KeyProduct{}).(data.Product)
 	p.l.Println("[DEBUG] Adding product to list")
 	data.AddProduct(&product)
-	//if err != nil {
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//	data.ToJSON(&GenericError{Message: "Couldn't create product ..."}, w)
-	//	p.l.Println("[ERROR] Unable to parse values to JSON ...", err)
-	//	return
-	//}
 	w.WriteHeader(http.StatusCreated)
 	data.ToJSON(&GenericError{Message: "Product created successfully!"}, w)
 }
